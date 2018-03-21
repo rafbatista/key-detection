767207f2c3cbea562b0b1c985e04d06812def1c5
rafbatista <rah5024@gmail.com>
2018-03-20 23:50:14 -0700

    User can recognize when key code is entered. #1

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 const keyed = []
 const code = 'howdy'
 
-window.addEventListener('keydown', e => {})
+window.addEventListener('keydown', event => {
+  keyed.push(event.key)
+  keyed.splice(-code.length - 1, keyed.length - code.length)
+  if (keyed.join('').includes(code)) {
+    alert('Secret Code Activated')
+    cornify_add()
+  }
+})
